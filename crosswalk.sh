7dd6e7dfe59b7d0488ff4c46b3411d95f21a8636
Heorhiy Kharvat <zaynyatyi@gmail.com>
2016-06-20 12:50:05 +0300

    Switched to newer version of library

--- crosswalk.sh
+++ crosswalk.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-ver="17.46.448.10"
+ver="18.48.477.13"
+beta=""
 
-wget https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/${ver}/xwalk_core_library-${ver}.aar
-unzip -j xwalk_core_library-${ver}.aar classes.jar
+wget https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library${beta}/${ver}/xwalk_core_library${beta}-${ver}.aar
+unzip -j xwalk_core_library${beta}-${ver}.aar classes.jar
 zip -d classes.jar javax\*
-zip -r xwalk_core_library-${ver}.aar classes.jar
+zip -r xwalk_core_library${beta}-${ver}.aar classes.jar
 rm -f classes.jar
-mv xwalk_core_library-${ver}.aar libs/
+mv xwalk_core_library${beta}-${ver}.aar libs/
